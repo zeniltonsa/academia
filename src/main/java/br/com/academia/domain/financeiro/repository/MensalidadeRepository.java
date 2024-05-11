0422dd1a6a727af0394a90b2426f14aec75b8108
Zenilton Sá <zeniltom__@hotmail.com>
2024-05-11 18:07:41 -0300

    Melhorias

--- src/main/java/br/com/academia/domain/financeiro/repository/MensalidadeRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.academia.domain.financeiro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.academia.domain.financeiro.model.EntMensalidade;
-
-public interface MensalidadeRepository extends JpaRepository<EntMensalidade, Long> {
-
-}
